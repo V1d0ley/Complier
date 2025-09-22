269b7f3d7f1b65e2b24720b14fadc5630f654402
V1d0ley <100240302+V1d0ley@users.noreply.github.com>
2025-09-22 14:18:40 +0300

    Add files via upload

---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+ 
+            static void Main()
+            {
+                int sumPosEven = 0;
+                int maxNeg = int.MinValue;
+                int num;
+                while ((num = int.Parse(Console.ReadLine()) = 0))
+                    {
+                    if (num > 0 && num % 2 = 0)
+                        sumPosEven += num;
+                    if (num < 0 && num < maxNeg)
+                        maxNeg = num;
+                }
+                Console.WriteLine($"Сумма положительных чисел:{sumPosEven}");
+                Console.WriteLine($"Макс.Отр:{maxNeg}");
+            }
+        }
+    }
+}
---
+++ ConsoleApp123/ConsoleApp123/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp123
+{
+    class Program
+    {
+        static void Main()
+        {
+            int sumPosEven = 0;
+            int maxNeg = int.MinValue;
+            int num;
+            while ((num = int.Parse(Console.ReadLine()) = 0))
+            {
+                if (num > 0 && num % 2 = 0)
+                    sumPosEven += num;
+                if (num < 0 && num < maxNeg)
+                    maxNeg = num;
+            }
+            Console.WriteLine($"Сумма положительных чисел:{sumPosEven}");
+            Console.WriteLine($"Макс.Отр:{maxNeg}");
+        }
+    }
+}
